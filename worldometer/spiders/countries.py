4f1789f62e85d932b815c1c07099c17173a1a187
mani <manihereatcusat@gmail.com>
2022-06-26 15:14:02 +0530

    country spider is created

---
+++ worldometer/spiders/countries.py
@@ -0,0 +1,17 @@
+import scrapy
+
+
+class CountriesSpider(scrapy.Spider):
+    name = 'countries'
+    allowed_domains = ['www.worldometers.info']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+
+    def parse(self, response : scrapy.Selector):
+
+        title = response.xpath('//h1/text()').get()
+        countries = response.xpath('//td/a/text()').getall()
+
+        yield {
+            "title" : title,
+            "countries" : countries
+        }
